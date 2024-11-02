97fc1bbf7ada85085aabe9fb322dd260aad53148
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-11-02 09:29:23 +0800

    effect fix

--- src/main/java/dev/xkmc/arsdelight/content/effect/WildenEffect.java
+++ src/main/java/dev/xkmc/arsdelight/content/effect/WildenEffect.java
@@ -1,6 +1,7 @@
 package dev.xkmc.arsdelight.content.effect;
 
 import com.hollingsworth.arsnouveau.api.perk.PerkAttributes;
+import com.hollingsworth.arsnouveau.setup.config.ServerConfig;
 import dev.xkmc.l2library.util.math.MathHelper;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
@@ -13,9 +14,10 @@ public class WildenEffect extends MobEffect {
 	public WildenEffect(MobEffectCategory pCategory, int pColor) {
 		super(pCategory, pColor);
 		UUID id = MathHelper.getUUIDFromString("arsdelight_wilden_effect");
-		addAttributeModifier(PerkAttributes.MAX_MANA.get(), id.toString(), 0.2, AttributeModifier.Operation.MULTIPLY_TOTAL);
-		addAttributeModifier(PerkAttributes.MANA_REGEN_BONUS.get(), id.toString(), 0.2, AttributeModifier.Operation.MULTIPLY_TOTAL);
-		addAttributeModifier(PerkAttributes.SPELL_DAMAGE_BONUS.get(), id.toString(), 0.2, AttributeModifier.Operation.MULTIPLY_TOTAL);
+		addAttributeModifier(PerkAttributes.MANA_REGEN_BONUS.get(), id.toString(),
+				ServerConfig.MANA_REGEN_POTION.get(), AttributeModifier.Operation.ADDITION);
+		addAttributeModifier(PerkAttributes.MAX_MANA.get(), id.toString(), 0.2,
+				AttributeModifier.Operation.MULTIPLY_TOTAL);
 	}
 
 }
