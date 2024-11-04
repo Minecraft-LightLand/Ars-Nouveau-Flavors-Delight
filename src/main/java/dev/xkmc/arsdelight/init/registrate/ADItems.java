477f0e8653c784f0b93f7314e4e066d6aa7b1975
lcy0x1 <chengyilin@umail.ucsb.edu>
2024-11-04 22:30:32 +0800

    lang fix

--- src/main/java/dev/xkmc/arsdelight/init/registrate/ADItems.java
+++ src/main/java/dev/xkmc/arsdelight/init/registrate/ADItems.java
@@ -38,7 +38,8 @@ public class ADItems {
 
 		KNIFE = ArsDelight.REGISTRATE.item("enchanters_knife", p -> new EnchantersKnife(Tiers.NETHERITE, 1, -2F))
 				.model((ctx, pvd) -> {
-				}).tag(ForgeTags.TOOLS_KNIVES, ModTags.KNIVES).register();
+				}).tag(ForgeTags.TOOLS_KNIVES, ModTags.KNIVES)
+				.lang("Enchanter's Knife").register();
 	}
 
 	public static String toEnglishName(String internalName) {
